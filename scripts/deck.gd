76351f08aad5a84aebe67de1a9b86cd192df0306
Sky Chan <s0ra@users.noreply.github.com>
2024-07-28 00:16:17 +0800

    Folder structures

---
+++ scripts/deck.gd
@@ -0,0 +1,16 @@
+extends Node
+
+# fire pol food str
+var CitiesStats = [[5.0, 5.0, 5.0, 5.0], [5.0, 5.0, 5.0, 5.0]]
+
+var CardEffect = [0, 0.0, 0, 0.0]
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
---
+++ scripts/game.gd
@@ -0,0 +1,15 @@
+extends Node2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	if Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT):
+		get_node("Card").modulate = Color.RED
+		#get_node("Card").
+		
+	pass
